9bcd0a2c3603c309bd1c3449e49757f398be6820
Андрей <Rhymes.un.limit@gmail.com>
2025-09-28 16:54:33 +0300

    creating view buttons

--- The Tic Tac Toe game/ViewController.swift
+++ The Tic Tac Toe game/ViewController.swift
@@ -3,12 +3,30 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
     
+    var currentPlayer = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        for button in buttons {
+            button.titleLabel?.font = .systemFont(ofSize: 72, weight: .bold)
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.black.cgColor
+        }
+        
+        
+    }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.setTitle(currentPlayer, for: .normal)
+        sender.isEnabled = false
+        
+        currentPlayer = currentPlayer == "X" ? "O" : "X"
     }
     
 }
