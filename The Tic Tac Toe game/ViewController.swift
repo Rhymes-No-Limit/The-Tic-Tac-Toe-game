721776382484fc1f446f6ffc0113cb7b7ec9e951
Андрей <Rhymes.un.limit@gmail.com>
2025-09-28 13:00:30 +0300

    Initial Commit

---
+++ The Tic Tac Toe game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Tic Tac Toe game
+//
+//  Created by Андрей on 28.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
