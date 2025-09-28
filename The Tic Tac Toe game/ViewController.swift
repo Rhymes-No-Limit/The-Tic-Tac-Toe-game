6d2639c9fc349a39d2347b4a2e8af028af6090a4
Андрей <Rhymes.un.limit@gmail.com>
2025-09-28 13:37:45 +0300

    prepare project, add elements

--- The Tic Tac Toe game/ViewController.swift
+++ The Tic Tac Toe game/ViewController.swift
@@ -1,19 +1,15 @@
-//
-//  ViewController.swift
-//  The Tic Tac Toe game
-//
-//  Created by Андрей on 28.09.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
 }
 
