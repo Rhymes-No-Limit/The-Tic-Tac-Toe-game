e0d1c29062038d9a17c6c9429fa35da679a44b4c
Андрей <Rhymes.un.limit@gmail.com>
2025-10-06 18:26:41 +0300

    creating back button from new game

--- The Tic Tac Toe game/NewGameViewController.swift
+++ The Tic Tac Toe game/NewGameViewController.swift
@@ -9,11 +9,29 @@ class NewGameViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         view.setGradientBackground(colors: [UIColor.lightGray.withAlphaComponent(0.8), UIColor.lightGray.withAlphaComponent(0.4)])
+        
+        let backButton = UIButton(type: .system)
+            backButton.setTitle("←", for: .normal)
+            backButton.titleLabel?.font = UIFont.systemFont(ofSize: 40, weight: .medium)
+            backButton.setTitleColor(.white, for: .normal)
+            backButton.translatesAutoresizingMaskIntoConstraints = false
+            backButton.addTarget(self, action: #selector(backToMenu), for: .touchUpInside)
+            
+            view.addSubview(backButton)
+            
+            NSLayoutConstraint.activate([
+                backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+                backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+                backButton.widthAnchor.constraint(equalToConstant: 100),
+                backButton.heightAnchor.constraint(equalToConstant: 40)
+            ])
+        
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showGameFromNewGame" {
-            if let gameVC = segue.destination as? ViewController {
+            if let gameVC = segue.destination as? gameViewController {
                 let playerName = nameTextField.text ?? "Игрок"
                 let modeIndex = modeSegment.selectedSegmentIndex
                 
@@ -25,6 +43,10 @@ class NewGameViewController: UIViewController {
     
     @IBAction func startGameTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "showGameFromNewGame", sender: nil)
+    }
+    
+    @objc private func backToMenu() {
+        dismiss(animated: false)
     }
     
  }
