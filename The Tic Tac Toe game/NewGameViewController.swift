b18631f6c035401bf278b62e62fc823d55b94879
Андрей <Rhymes.un.limit@gmail.com>
2025-11-01 14:18:55 +0300

    Current entering name first person

--- The Tic Tac Toe game/NewGameViewController.swift
+++ The Tic Tac Toe game/NewGameViewController.swift
@@ -45,7 +45,32 @@ final class NewGameViewController: UIViewController {
     }
     
     @IBAction func startGameTapped(_ sender: UIButton) {
+        guard let name = nameTextField.text, !name.isEmpty else {
+            showAlert(message: "Введите имя перед началом игры.", clearField: true)
+        return
+        }
+        
+        let regex = "^[A-Za-z]+$"
+        let isValid = NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: name)
+        
+        guard isValid else {
+            showAlert(message: "Имя должно состоять только из латинских букв.", clearField: true)
+            return
+        }
+        
         performSegue(withIdentifier: "showGameFromNewGame", sender: self)
+    }
+    
+    private func showAlert(message: String, clearField: Bool = false) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ок", style: .default) { [weak self] _ in
+            if clearField {
+                self?.nameTextField.text = ""
+            }
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
     }
     
     @objc private func backToMenu() {
